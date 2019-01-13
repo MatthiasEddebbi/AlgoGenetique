6db3a041a377f75df4ce74a2767532c26df0743c
MatthiasEddebbi <33234811+MatthiasEddebbi@users.noreply.github.com>
2019-01-13 12:24:46 +0100

    Genetic Manager now Singleton

--- GeneticAlgoFramework/src/main/GeneticManager.java
+++ GeneticAlgoFramework/src/main/GeneticManager.java
@@ -1,5 +1,22 @@
 package main;
 
+/**
+ * 
+ * @author matth
+ * Manager of genetic algorithme. Centralize every step of the processing
+ */
 public class GeneticManager {
-
+	
+	private static GeneticManager instance = new GeneticManager();
+	
+	private GeneticManager() {
+		
+	}
+	
+	public static GeneticManager getInstance() {
+		return instance;
+	}
+	
+	
+    
 }
