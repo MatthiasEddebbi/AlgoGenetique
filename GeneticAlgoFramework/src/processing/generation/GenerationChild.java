7ae14f3690bfaa98ee011af3fd810af9e05a6827
Alex Charbonnier <alexcharb@gmail.com>
2019-01-13 15:24:46 +0100

    GenerationChild Amen

--- GeneticAlgoFramework/src/processing/generation/GenerationChild.java
+++ GeneticAlgoFramework/src/processing/generation/GenerationChild.java
@@ -1,5 +1,6 @@
 package processing.generation;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import main.Individual;
@@ -7,27 +8,31 @@ import main.Individual;
 public class GenerationChild {
 	
 	protected double mutationRate;
+	protected int wantedChild; //cannot be more than the number of parents - 1 
 
-	public GenerationChild(double _mutationRate) {
+	public GenerationChild(double _mutationRate, int _wantedChild) {
 
 		mutationRate = _mutationRate;
+		wantedChild = _wantedChild;
 	}
 	
 	public List<Individual> GenerateChildList(List<Individual> individualList){
 		double threshold;
-		int i;
-		List<Individual> childList = null;
-		for (i=0; i<individualList.size()-1; i++)
-		{
-			threshold = (double)(Math.random() * (individualList.size() + 1));
-			if (threshold <= this.mutationRate) {
-				//TODO
-			}
-			else {
-				//TODO
-			}
-		}
+		int i, j, k;
+		Individual individualChild;
+		List<Individual> childList = new ArrayList<Individual>();
 		
+		for (i=0; i<wantedChild; i++)
+		{
+			j = (int)(Math.random() * (individualList.size() + 1));
+			k = (int)(Math.random() * (individualList.size() + 1));
+			individualChild = individualList.get(j).Crossing(individualList.get(k));
+			threshold = (double)(Math.random() * (100 + 1));
+			if (threshold <= this.mutationRate) {
+				individualChild.Mutate();
+			}
+			childList.add(individualChild);
+		}
 		return childList;
 	}
 }
