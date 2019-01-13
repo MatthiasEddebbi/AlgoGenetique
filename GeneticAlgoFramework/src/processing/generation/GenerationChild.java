5d4b518c433a2f945661dda8098f418a35a6e195
Alex Charbonnier <alexcharb@gmail.com>
2019-01-13 14:48:17 +0100

    Start of GenerateChild
    tell me bout it

--- GeneticAlgoFramework/src/processing/generation/GenerationChild.java
+++ GeneticAlgoFramework/src/processing/generation/GenerationChild.java
@@ -1,5 +1,35 @@
 package processing.generation;
 
-public class GenerationChild {
+import java.util.List;
 
+import main.Individual;
+
+public class GenerationChild {
+	
+	private List<Individual> individualList;
+	protected double mutationRate;
+
+	public GenerationChild(List<Individual> _individualList, double _mutationRate) {
+
+		individualList = _individualList;
+		mutationRate = _mutationRate;
+	}
+	
+	public List<Individual> GenerateChildList(){
+		double threshold;
+		int i;
+		List<Individual> childList = null;
+		for (i=0; i<this.individualList.size()-1; i++)
+		{
+			threshold = (double)(Math.random() * (this.individualList.size() + 1));
+			if (threshold <= this.mutationRate) {
+				//TODO
+			}
+			else {
+				//TODO
+			}
+		}
+		
+		return childList;
+	}
 }
