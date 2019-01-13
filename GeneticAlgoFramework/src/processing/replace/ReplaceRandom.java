ed154e95af089711eb082f618bcc6dd0e143e14e
Alex Charbonnier <alexcharb@gmail.com>
2019-01-13 15:42:10 +0100

    Replace random v1

--- GeneticAlgoFramework/src/processing/replace/ReplaceRandom.java
+++ GeneticAlgoFramework/src/processing/replace/ReplaceRandom.java
@@ -1,5 +1,22 @@
 package processing.replace;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import main.Individual;
+
 public class ReplaceRandom extends ReplaceStrategy{
 
+	public List<Individual> Replace(List<Individual> parentList, List<Individual> childList){
+		
+		int i, rand;
+
+		for (i=0; i<childList.size(); i++) {
+			
+			rand = 1 + (int)(Math.random() * (parentList.size() + 1));
+			parentList.add(rand, childList.get(i));
+		}
+		
+		return parentList;
+	}
 }
--- GeneticAlgoFramework/src/processing/replace/ReplaceStrategy.java
+++ GeneticAlgoFramework/src/processing/replace/ReplaceStrategy.java
@@ -1,5 +1,18 @@
 package processing.replace;
 
+import java.util.List;
+
+import main.Individual;
+
 public abstract class ReplaceStrategy {
 
+	/**
+	 * Abstract constructor for the Replace step
+	 * @param List<Individual> ind 
+	 */
+	
+	public ReplaceStrategy() {
+	}
+	
+	public abstract List<Individual> Replace(List<Individual> parentList, List<Individual> childList);
 }
