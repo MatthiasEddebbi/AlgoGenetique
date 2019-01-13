6be33ac893e5eeb1194fab07b193587cf8b5b630
Alex Charbonnier <alexcharb@gmail.com>
2019-01-13 15:44:03 +0100

    Replace Best

--- GeneticAlgoFramework/src/processing/replace/ReplaceBest.java
+++ GeneticAlgoFramework/src/processing/replace/ReplaceBest.java
@@ -1,5 +1,15 @@
 package processing.replace;
 
+import java.util.List;
+
+import main.Individual;
+
 public class ReplaceBest extends ReplaceStrategy{
 
+	public List<Individual> Replace(List<Individual> parentList, List<Individual> childList){
+		//TODO
+		return parentList;
+	}
+	
+	
 }
