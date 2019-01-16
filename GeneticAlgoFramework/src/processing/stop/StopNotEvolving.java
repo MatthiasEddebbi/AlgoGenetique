8c2624fe2b8b7cd5a668849f83794483d1edcd7a
DURANDEAU <alexandredurandeau@gmail.com>
2019-01-16 17:01:10 +0100

    StopNotEvolving

--- GeneticAlgoFramework/src/processing/stop/StopNotEvolving.java
+++ GeneticAlgoFramework/src/processing/stop/StopNotEvolving.java
@@ -1,5 +1,35 @@
 package processing.stop;
+import java.util.List;
 
-public class StopNotEvolving {
+import main.GeneticManager;
+import main.Individual;
+
+public class StopNotEvolving extends StopStrategy {
+	
+	private int iterations;
+	private int nbIterations = 0;
+	private GeneticManager gm;
+	private List<Individual> oldGeneration;
+	
+	public StopNotEvolving(int iterations)
+	{
+		oldGeneration = gm.getInstance().getCurrentGeneration();
+		this.iterations = iterations;
+	}
+	
+	public boolean Stop()
+	{
+		nbIterations ++;
+		
+		if (gm.getInstance().getCurrentGeneration() == oldGeneration && nbIterations == iterations)
+		{
+			return true;
+		}
+		else
+		{
+			oldGeneration = gm.getInstance().getCurrentGeneration();
+			return false;
+		}
+	}
 
 }
